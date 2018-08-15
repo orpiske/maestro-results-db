a56e42cbd8add1b25fd1ff95ccfaa4fc8b45921d
Otavio Rodolfo Piske <opiske@redhat.com>
2018-08-15 13:42:58 +0200

    UPD: simplified the table building code

--- maestro-results-server/src/main/resources/site/maestro/js/sut.table.js
+++ maestro-results-server/src/main/resources/site/maestro/js/sut.table.js
@@ -1,71 +1,17 @@
 
 $(document).ready(function() {
-    var dataSet = [];
-
-    axios.get('/api/sut')
-      .then(function (response) {
-        // handle success
-//        console.log(response);
-
-        dataSet = response.data;
-
-        $('#suttable').DataTable({
-            columns: [
-                { data: "sutId" },
-                { data: "sutName" },
-                { data: "sutVersion" },
-                { data: "sutJvmInfo" },
-                { data: "sutOther"},
-                { data: "sutTags"}
-            ],
-            data: dataSet,
-            dom: "t",
-            language: {
-                zeroRecords: "No records found"
-            },
-            order: [[ 1, 'asc' ]],
-            pfConfig: {
-                emptyStateSelector: "#emptyState",
-                filterCaseInsensitive: true,
-                filterCols: [
-                    null,
-                    {
-                        default: true,
-                        optionSelector: "#filter",
-                        placeholder: "Filter By Name..."
-                    }
-                ],
-//                paginationSelector: "#pagination1",
-//                toolbarSelector: "#toolbar1",
-//                colvisMenuSelector: '.table-view-pf-colvis-menu'
-            }
-        });
-
-//        console.log(dataSet);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-
-
-    var emptyTableViewUtil = function (config) {
-        var self = this;
-
-        this.dt = $(config.tableSelector).DataTable(); // DataTable
-
-        // Initialize restore rows
-        if (this.dt.data().length === 0) {
-            $(this.restoreRows).prop("disabled", false);
+    $('#suttable').DataTable({
+        columns: [
+            { data: "sutId" },
+            { data: "sutName" },
+            { data: "sutVersion" },
+            { data: "sutJvmInfo" },
+            { data: "sutOther"},
+            { data: "sutTags"}
+        ],
+        ajax: {
+            url: '/api/sut/',
+            dataSrc:  ''
         }
-    };
-
-    // Initialize empty Table View util
-    new emptyTableViewUtil({
-        data: dataSet,
-        tableSelector: "#suttable"
     });
 });
