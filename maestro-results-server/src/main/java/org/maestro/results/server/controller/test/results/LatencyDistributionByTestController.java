1a9828baefd173eec4a67b50e0df249d8a2bdf70
Otavio Rodolfo Piske <opiske@redhat.com>
2018-09-05 11:47:34 +0200

    NEW: support for displaying the latency evolution over test iterations

---
+++ maestro-results-server/src/main/java/org/maestro/results/server/controller/test/results/LatencyDistributionByTestController.java
@@ -0,0 +1,57 @@
+package org.maestro.results.server.controller.test.results;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.javalin.Context;
+import io.javalin.Handler;
+import org.maestro.common.HostTypes;
+import org.maestro.results.dao.TestResultsDao;
+import org.maestro.results.dto.TestResult;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LatencyDistributionByTestController implements Handler {
+    public class LatPair {
+        @JsonProperty("Test Number")
+        int testNumber;
+
+        @JsonProperty("90th percentile")
+        double latPercentile90;
+
+        @JsonProperty("95th percentile")
+        double latPercentile95;
+
+        @JsonProperty("99th percentile")
+        double latPercentile99;
+
+        public LatPair(final TestResult testResult) {
+            this(testResult.getTestNumber(), testResult.getLatPercentile90() / 1000,
+                    testResult.getLatPercentile95() / 1000, testResult.getLatPercentile99() / 1000);
+        }
+
+        private LatPair(int testNumber, double latPercentile90, double latPercentile95, double latPercentile99) {
+            this.testNumber = testNumber;
+            this.latPercentile90 = latPercentile90;
+            this.latPercentile95 = latPercentile95;
+            this.latPercentile99 = latPercentile99;
+        }
+
+    }
+
+    private TestResultsDao testResultsDao = new TestResultsDao();
+
+    @Override
+    public void handle(Context context) throws Exception {
+        int id = Integer.parseInt(context.param("id"));
+
+        List<TestResult> testResultList = testResultsDao.fetch(id, HostTypes.RECEIVER_HOST_TYPE);
+
+        List<LatPair> combined = new LinkedList<>();
+
+        for (TestResult testResult : testResultList) {
+            combined.add(new LatPair(testResult));
+        }
+
+        context.json(combined);
+    }
+}
---
+++ maestro-results-server/src/main/resources/site/maestro/js/maestro.graph.lat.percentiles.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+//    require("axios")
+
+    var id = getUrlVars()["test-id"]
+
+    var url = $('[data-datatables]').attr('data-api') + "/latency/" + id
+
+    axios.get(url).then(function (response) {
+        var chartData = response.data
+
+        console.log(response);
+
+        console.log(chartData);
+
+        var c3ChartDefaults = $().c3ChartDefaults();
+        var lineChartConfig = c3ChartDefaults.getDefaultLineConfig();
+        lineChartConfig.bindto = '#line-chart-3';
+
+        // Latency distributions per test
+        lineChartConfig.data = {
+            json: chartData,
+            keys: {
+                x: 'Test Number', // it's possible to specify 'x' when category axis
+                value: ['90th percentile', '95th percentile', '99th percentile']
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    label: {
+                        text: 'Latency percentiles',
+                    },
+                },
+                y: {
+                    label: {
+                        text: 'Milliseconds',
+                    }
+                }
+            }
+        };
+
+        var lineChart = c3.generate(lineChartConfig);
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
+    }
+)
+
+
+
