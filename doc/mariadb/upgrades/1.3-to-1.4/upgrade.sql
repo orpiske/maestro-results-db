78a53e9c0bb02ef119de3c69e253b850e2276359
Otavio Rodolfo Piske <opiske@redhat.com>
2018-09-10 08:55:53 +0200

    UPD: additional data design updates for the upgrade

--- doc/mariadb/upgrades/1.3-to-1.4/upgrade.sql
+++ doc/mariadb/upgrades/1.3-to-1.4/upgrade.sql
@@ -20,13 +20,29 @@ create or replace view `test_result_statistics` as
 alter table env_results modify column lat_percentile_90 DOUBLE default 0;
 alter table env_results modify column lat_percentile_95 DOUBLE default 0;
 alter table env_results modify column lat_percentile_99 DOUBLE default 0;
-    
+
 update env_results set lat_percentile_90 = 0 where lat_percentile_90 is null and env_resource_role = 'receiver';
 update env_results set lat_percentile_95 = 0 where lat_percentile_95 is null and env_resource_role = 'receiver';
 update env_results set lat_percentile_99 = 0 where lat_percentile_99 is null and env_resource_role = 'receiver';
 
 update env_results set lat_percentile_90 = 0,lat_percentile_95 = 0,lat_percentile_99 = 0 where env_resource_role = 'sender';
 
+-- Recreate the unified test properties view
+CREATE OR REPLACE VIEW `test_properties` AS
+select
+    tmp.test_id,
+    tmp.test_number,
+    MAX(IF(tmp.test_msg_property_name = "apiName", tmp.test_msg_property_value, NULL)) AS api_name,
+    MAX(IF(tmp.test_msg_property_name = "apiVersion", tmp.test_msg_property_value, NULL)) AS api_version,
+    MAX(IF(tmp.test_msg_property_name = "durable", IF(tmp.test_msg_property_value = "true", true, false), NULL)) AS durable,
+    MAX(IF(tmp.test_msg_property_name = "limitDestinations", CAST(tmp.test_msg_property_value AS INTEGER), NULL)) AS limit_destinations,
+    MAX(IF(tmp.test_msg_property_name = "messageSize", CAST(tmp.test_msg_property_value AS INTEGER), NULL)) AS message_size,
+    MAX(IF(tmp.test_msg_property_name = "protocol", tmp.test_msg_property_value, NULL)) AS messaging_protocol,
+    MAX(IF(tmp.test_msg_property_name = "variableSize", IF(tmp.test_msg_property_value = "1", true, false), NULL)) AS variable_size,
+    MAX(IF(tfc.test_fail_condition_name = "fcl", CAST(tfc.test_fail_condition_value AS INTEGER), NULL)) AS max_acceptable_latency
+  from test_msg_property tmp, test_fail_condition tfc
+  where tmp.test_id = tfc.test_id and tmp.test_number = tfc.test_number
+  group by test_id,test_number;
 
 -- recreate the test_results view
 CREATE OR REPLACE VIEW `test_results` AS
