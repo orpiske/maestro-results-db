f1548401a8421a98f8c281a1dc77e0e931cc5bf7
Otavio Rodolfo Piske <opiske@redhat.com>
2018-09-12 07:09:47 +0200

    UPD: enabled cache support for EnvResourceDao

--- maestro-results-dao/src/main/java/org/maestro/results/dao/EnvResourceDao.java
+++ maestro-results-dao/src/main/java/org/maestro/results/dao/EnvResourceDao.java
@@ -1,11 +1,45 @@
 package org.maestro.results.dao;
 
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.maestro.results.dto.EnvResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
+import java.time.Duration;
 import java.util.List;
 
 public class EnvResourceDao extends AbstractDao {
+    private static final Logger logger = LoggerFactory.getLogger(EnvResourceDao.class);
+    private static CacheManager cacheManager;
+    private static Cache<Integer, List> cache;
+
+    public EnvResourceDao() {
+        if (cacheManager == null) {
+            synchronized (this) {
+                if (cacheManager == null) {
+                    CacheConfigurationBuilder<Integer, List> config = CacheConfigurationBuilder
+                            .newCacheConfigurationBuilder(Integer.class, List.class, ResourcePoolsBuilder.heap(30))
+                            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(5)));
+
+                    cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                            .withCache("envRes",config)
+                            .build();
+
+                    cacheManager.init();
+
+                    cache = cacheManager.getCache("envRes", Integer.class, List.class);
+                }
+            }
+        }
+
+    }
+
     public int insert(EnvResource dto) {
         return runInsert(
                 "insert into env_resource(env_resource_name, env_resource_os_name, env_resource_os_arch, " +
@@ -36,10 +70,22 @@ public class EnvResourceDao extends AbstractDao {
     }
 
     public List<EnvResource> fetchForTest(int testId) {
-        return jdbcTemplate.query("select * from env_resource where env_resource_name in (" +
-                "select env_resource_name from test_results where test_id = ? group by env_resource_name)",
-                new Object[] { testId },
-                new BeanPropertyRowMapper<>(EnvResource.class));
+        List<EnvResource> ret = cache.get(testId);
 
+        if (ret == null) {
+            logger.warn("Record not in cache. Retrieving from the DB");
+
+            ret = jdbcTemplate.query("select * from env_resource where env_resource_name in (" +
+                            "select env_resource_name from test_results where test_id = ? group by env_resource_name)",
+                    new Object[]{testId},
+                    new BeanPropertyRowMapper<>(EnvResource.class));
+
+            cache.put(testId, ret);
+        }
+        else {
+            logger.info("Returning cached result");
+        }
+
+        return ret;
     }
 }
