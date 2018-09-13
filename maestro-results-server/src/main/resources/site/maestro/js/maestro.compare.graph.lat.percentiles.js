2513703f917544e7821787476f08dbbf2ed11a2c
Otavio Rodolfo Piske <opiske@redhat.com>
2018-09-13 09:32:58 +0200

    DEL: commented code

--- maestro-results-server/src/main/resources/site/maestro/js/maestro.compare.graph.lat.percentiles.js
+++ maestro-results-server/src/main/resources/site/maestro/js/maestro.compare.graph.lat.percentiles.js
@@ -6,42 +6,6 @@ $(document).ready(function() {
 
     var url = $('[graphs]').attr('graph-api') + tId0 + '/' + tNum0 + '/' + tId1 + '/' + tNum1 + '/'
 
-//    axios.get(url).then(function (response) {
-//        var chartData = response.data
-//
-//        var c3ChartDefaults = $().c3ChartDefaults();
-//        var lineChartConfig = c3ChartDefaults.getDefaultLineConfig();
-//        lineChartConfig.bindto = '#line-chart-3';
-//
-//        // Latency distributions per test
-//        lineChartConfig.data = {
-//            json: chartData,
-//            keys: {
-//                x: 'Test Number', // it's possible to specify 'x' when category axis
-//                value: ['90th percentile']
-//            },
-//            axis: {
-//                x: {
-//                    type: 'category',
-//                    label: {
-//                        text: 'Latency percentiles',
-//                    },
-//                },
-//                y: {
-//                    label: {
-//                        text: 'Milliseconds',
-//                    }
-//                }
-//            }
-//        };
-//
-//        var lineChart = c3.generate(lineChartConfig);
-//
-//      })
-//      .catch(function (error) {
-//        console.log(error);
-//      });
-
     axios.get(url).then(function (response) {
         var chartData = response.data
 
