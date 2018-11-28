85cdb1cc01d0b2c863915adc1c1739b44373b2fb
Otavio Rodolfo Piske <opiske@redhat.com>
2018-11-28 15:06:09 +0100

    FIX: ensure that the test record ID matches the one from the reports table

--- maestro-results-dao/src/main/java/org/maestro/results/dao/TestDao.java
+++ maestro-results-dao/src/main/java/org/maestro/results/dao/TestDao.java
@@ -16,10 +16,10 @@ public class TestDao extends AbstractDao {
 
     public int insert(final Test dto) {
         return runInsert(
-                "insert into test(test_name, test_number, test_result, sut_id, " +
+                "insert into test(test_id, test_name, test_number, test_result, sut_id, " +
                         "test_report_link, test_data_storage_info, test_tags, test_date, test_duration, test_target_rate, " +
                         "maestro_version) " +
-                        "values(:testName, :testNumber, :testResult, :sutId, :testReportLink," +
+                        "values(:testId, :testName, :testNumber, :testResult, :sutId, :testReportLink," +
                         ":testDataStorageInfo, :testTags, now(), :testDuration, :testTargetRate, :maestroVersion)", dto);
     }
 
