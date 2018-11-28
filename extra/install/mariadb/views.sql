975b2801b5b87c51d5f3901ac3806048bca1ece8
Otavio Rodolfo Piske <opiske@redhat.com>
2018-11-28 15:00:28 +0100

    FIX: also use saner default values for other message properties

--- extra/install/mariadb/views.sql
+++ extra/install/mariadb/views.sql
@@ -5,11 +5,11 @@ select
     MAX(IF(tmp.test_msg_property_name = "apiName", tmp.test_msg_property_value, NULL)) AS api_name,
     MAX(IF(tmp.test_msg_property_name = "apiVersion", tmp.test_msg_property_value, NULL)) AS api_version,
     MAX(IF(tmp.test_msg_property_name = "durable", IF(tmp.test_msg_property_value = "true", true, false), false)) AS durable,
-    MAX(IF(tmp.test_msg_property_name = "limitDestinations", CAST(tmp.test_msg_property_value AS INTEGER), NULL)) AS limit_destinations,
-    MAX(IF(tmp.test_msg_property_name = "messageSize", CAST(tmp.test_msg_property_value AS INTEGER), NULL)) AS message_size,
+    MAX(IF(tmp.test_msg_property_name = "limitDestinations", CAST(tmp.test_msg_property_value AS INTEGER), -1)) AS limit_destinations,
+    MAX(IF(tmp.test_msg_property_name = "messageSize", CAST(tmp.test_msg_property_value AS INTEGER), -1)) AS message_size,
     MAX(IF(tmp.test_msg_property_name = "protocol", tmp.test_msg_property_value, NULL)) AS messaging_protocol,
-    MAX(IF(tmp.test_msg_property_name = "variableSize", IF(tmp.test_msg_property_value = "1", true, false), NULL)) AS variable_size,
-    MAX(IF(tfc.test_fail_condition_name = "fcl", CAST(tfc.test_fail_condition_value AS INTEGER), NULL)) AS max_acceptable_latency
+    MAX(IF(tmp.test_msg_property_name = "variableSize", IF(tmp.test_msg_property_value = "1", true, false), false)) AS variable_size,
+    MAX(IF(tfc.test_fail_condition_name = "fcl", CAST(tfc.test_fail_condition_value AS INTEGER), -1)) AS max_acceptable_latency
   from test_msg_property tmp, test_fail_condition tfc
   where tmp.test_id = tfc.test_id and tmp.test_number = tfc.test_number
   group by test_id,test_number;
